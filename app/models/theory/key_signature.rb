a4868eedd7321d01eb0da203a31db7fbe9b74656
Rob Head <robert.head@gmail.com>
2017-02-08 23:25:44 -0800

    Finish KeySignature class.

--- app/models/theory/key_signature.rb
+++ app/models/theory/key_signature.rb
@@ -1,12 +1,14 @@
 class Theory::KeySignature
-  attr_reader :tonic
+  attr_reader :tonic_spelling
   attr_reader :scale_type
 
   SHARPS = %w{F# C# G# D# A# E# B#}
   FLATS = %w{Bb Eb Ab Db Gb Cb Fb}
 
-  def initialize(tonic, scale_type = :major)
-    @tonic = tonic
+  delegate :pitch_class, to: :tonic_spelling, prefix: :tonic
+
+  def initialize(tonic_spelling, scale_type = :major)
+    @tonic_spelling = tonic_spelling
     @scale_type = scale_type
   end
 
@@ -19,23 +21,14 @@ class Theory::KeySignature
   end
 
   def num_sharps
-    (circle_pitch_classes.index(tonic) - scale_type_adjustment) % 12
+    (Theory::Circle.of_fifths.pitch_classes.index(tonic_pitch_class) - scale_type_adjustment) % 12
   end
 
   def num_flats
-    (circle_pitch_classes.reverse.index(tonic) + scale_type_adjustment) % 12
-  end
-
-  def default_spelling_type
-    return :flats if num_flats < num_sharps
-    :sharps
+    (Theory::Circle.of_fourths.pitch_classes.index(tonic_pitch_class) + scale_type_adjustment) % 12
   end
 
   private
-
-  def circle_pitch_classes
-    @circle_pitch_classes ||= Theory::Circle.new().pitch_classes
-  end
 
   def scale_type_adjustment
     scale_type == :minor ? 3 : 0
--- spec/models/theory/key_signature_spec.rb
+++ spec/models/theory/key_signature_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Theory::KeySignature do
-  subject(:key_signature) { Theory::KeySignature.new(pitch_class) }
+  subject(:key_signature) { Theory::KeySignature.new(tonic) }
 
   context 'in the key of C major' do
-    let(:pitch_class) { Theory::PitchClass.for_note_name('C') }
+    let(:tonic) { Theory::Spelling.get('C') }
 
     specify { expect(key_signature.num_sharps).to eq 0 }
     specify { expect(key_signature.num_flats).to eq 0 }
   end
 
   context 'in the key of D#/Eb major' do
-    let(:pitch_class) { Theory::PitchClass.for_note_name('Eb') }
+    let(:tonic) { Theory::Spelling.get('Eb') }
 
     specify { expect(key_signature.num_sharps).to eq 9 }
     specify { expect(key_signature.num_flats).to eq 3 }
